29b326bcc90cea43cc8fc84af05dfcf7e57aba48
imetandy <robinson.andrewjames@gmail.com>
2024-10-05 21:04:29 +0100

    adding kinobi support

---
+++ endcoin/tests/generate-clients.ts
@@ -0,0 +1,12 @@
+import { createFromRoot } from 'kinobi';
+import { rootNodeFromAnchor, AnchorIdl } from '@kinobi-so/nodes-from-anchor';
+import { renderVisitor as renderJavaScriptVisitor } from "@kinobi-so/renderers-js";
+import anchorIdl from '../target/idl/endcoin.json';
+import path from 'path';
+
+const kinobi = createFromRoot(rootNodeFromAnchor(anchorIdl as AnchorIdl));
+
+const jsClient = path.join(__dirname, "..", "clients", "js");
+kinobi.accept(
+  renderJavaScriptVisitor(path.join(jsClient, "src", "generated"))
+);
