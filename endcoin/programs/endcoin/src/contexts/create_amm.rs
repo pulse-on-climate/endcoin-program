845c3ad70184dda400fc08d9dac4386ce89fec8a
imetandy <robinson.andrewjames@gmail.com>
2025-01-03 10:45:39 +0000

    added logic to set the amm admin to the upgrade auth account, will add function to update this.

--- endcoin/programs/endcoin/src/contexts/create_amm.rs
+++ endcoin/programs/endcoin/src/contexts/create_amm.rs
@@ -1,13 +1,13 @@
 use anchor_lang::prelude::*;
-use crate::{constants::AMM_SEED, errors::*, state::Amm};
+use crate::{constants::AMM_SEED, errors::*, program::Endcoin, state::Amm};
 
 #[derive(Accounts)]
 #[instruction(id: Pubkey, fee: u16)]
 pub struct CreateAmm<'info> {
     // The AMM account
     #[account(
-        init_if_needed,
-        payer = payer,
+        init,
+        payer = authority,
         space = Amm::LEN,
         seeds = [
             AMM_SEED,
@@ -23,10 +23,13 @@ pub struct CreateAmm<'info> {
         constraint = admin.is_signer @ AmmError::UnauthorizedAdmin
     )]
     pub admin: Signer<'info>,
-
+    #[account(constraint = program.programdata_address()? == Some(program_data.key()))]
+    pub program: Program<'info, Endcoin>,
+    #[account(constraint = program_data.upgrade_authority_address == Some(authority.key()))]
+    pub program_data: Account<'info, ProgramData>,
     // The account paying for all rents
     #[account(mut)]
-    pub payer: Signer<'info>,
+    pub authority: Signer<'info>,
     // Solana ecosystem accounts
     pub system_program: Program<'info, System>,
 }
